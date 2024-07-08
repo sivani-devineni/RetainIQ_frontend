59ebee17423210f0318e0671a93aa3fc88b29f8c
sivani-devineni <98705928+sivani-devineni@users.noreply.github.com>
2024-07-08 16:04:03 +0530

    Updated NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,16 +5,12 @@ function App() {
   return (
     <>
       <AppBar position="fixed" style={{ backgroundColor: 'black', top: 0 }}>
-        <Toolbar>
-          {/* Content for the top AppBar */}
+        <Toolbar>         
         </Toolbar>
       </AppBar>
 
-      {/* Your main content here */}
-
       <AppBar position="fixed" style={{ backgroundColor: 'black', top: 'auto', bottom: 0 }}>
         <Toolbar>
-          {/* Content for the bottom AppBar */}
         </Toolbar>
       </AppBar>
     </>
